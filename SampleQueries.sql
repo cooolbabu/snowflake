299012d6b496eaa33e3e357c91d4fb4440d4ca5f
cooolbabu <cooolbabu@yahoo.com>
2022-12-12 13:40:03 -0600

    Added more queries

--- SampleQueries.sql
+++ SampleQueries.sql
@@ -1,2 +1,14 @@
 
-select * from GARDEN_PLANTS.VEGGIES.VEGETABLE_DETAILS;
+use role accountadmin;
+
+select current_account(), current_user();
+
+
+
+use schema snowflake.account_usage;
+
+select * from table (information_schema.warehouse_metering_history(dateadd('days',-10,current_date())));
+
+select * from  SNOWFLAKE.ACCOUNT_USAGE.WAREHOUSE_METERING_HISTORY;
+                     
+select * from query_history;
